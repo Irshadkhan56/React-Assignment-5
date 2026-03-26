da1293b2b9dd3d6f1524fbe3c45b17949fdfc40c
Irshadkhan56 <irshadkh23r@gmail.com>
2026-03-26 20:53:12 +0500

    Done second

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import Counter from "./Components/Counter";
-import ProductLike from "./components/ProductLike";
+import ProductLike from "./Components/ProductLike";
 
 const App = () => {
   return (
